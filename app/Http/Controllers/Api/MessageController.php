89fef2a8f9f7baef2f488a63ad3469c66290b234
umermuk <umerwell1234@gmail.com>
2024-03-19 06:30:37 +0500

    working

--- app/Http/Controllers/Api/MessageController.php
+++ app/Http/Controllers/Api/MessageController.php
@@ -380,45 +380,163 @@ class MessageController extends Controller
      */
     public function update(MessageRequest $request, Message $message)
     {
+        // try {
+        //     DB::beginTransaction();
+        //     $user_id = auth()->user()->id;
+        //     $contact = Contact::where('id', $request->contact_id)->first();
+        //     if (empty($contact))
+        //         throw new Error('Contact not found');
+        //     $receiver = User::where('email', $contact->email)->where('is_active', 1)->first();
+        //     if (empty($receiver))
+        //         throw new Error('First tell the person to register on this app and then you can add event');
+
+        //     $messageData = [
+        //         'user_id' => $user_id ?? '',
+        //         'contact_id' => $request->contact_id ?? '',
+        //         'gift_id' => $request->gift_id ?? null,
+        //         'message' => $request->message ?? '',
+        //         'video' => $request->video ?? '',
+        //         'is_draft' => 0,
+        //         'is_schedule' => 0,
+        //         'is_read' => 0,
+        //         'schedule_date' => date('Y-m-d', strtotime($request->date)),
+        //     ];
+
+        //     if (!empty($request->voice)) {
+        //         if (!empty($message->voice) && file_exists(public_path('storage/' . $message->voice)))
+        //             unlink(public_path('storage/' . $message->voice));
+        //         $voice = $request->voice;
+        //         $filename = "Voice-" . time() . "-" . rand() . "." . $voice->getClientOriginalExtension();
+        //         $voice->storeAs('message', $filename, "public");
+
+        //         $messageData['voice'] = "message/" . $filename;
+        //     }
+
+        //     $message->update($messageData);
+
+        //     DB::commit();
+        //     return response()->json([
+        //         'status' => true,
+        //         'message' => "Message Send Successfully",
+        //         'messages' => new AllMessageResource($message)
+        //     ]);
+        // } catch (Throwable $th) {
+        //     DB::rollBack();
+        //     return response()->json([
+        //         'status' => false,
+        //         'message' => $th->getMessage()
+        //     ], 500);
+        // }
         try {
             DB::beginTransaction();
+
             $user_id = auth()->user()->id;
             $contact = Contact::where('id', $request->contact_id)->first();
             if (empty($contact))
                 throw new Error('Contact not found');
+
             $receiver = User::where('email', $contact->email)->where('is_active', 1)->first();
             if (empty($receiver))
-                throw new Error('First tell the person to register on this app and then you can add event');
-
+                throw new Error('First tell the person must register on this app before you can add an event');
+            $type = $request->type ?? '';
+            if (empty($type) || empty($request->message))
+                throw new Error('Message Failed.');
+            // $chat = Chat::create([
+            //     'user_id' => $user_id,
+            //     'receiver_id' => $receiver->id,
+            //     'date' => date('Y-m-d', strtotime($request->date)),
+            // ]);
             $messageData = [
+                // 'chat_id' => $chat->id ?? '',
                 'user_id' => $user_id ?? '',
-                'contact_id' => $request->contact_id ?? '',
-                'gift_id' => $request->gift_id ?? null,
-                'message' => $request->message ?? '',
-                'video' => $request->video ?? '',
-                'is_draft' => 0,
-                'is_schedule' => 0,
-                'is_read' => 0,
+                'receiver_id' => $receiver->id ?? '',
+                // 'gift_id' => $request->gift_id ?? null,
+                'type' => $type,
+                'is_draft' => (!empty($request->draft) && $request->draft == 1) ? 1 : 0,
                 'schedule_date' => date('Y-m-d', strtotime($request->date)),
             ];
+            $messages = []; // Initialize an array to store messages
 
-            if (!empty($request->voice)) {
-                if (!empty($message->voice) && file_exists(public_path('storage/' . $message->voice)))
-                    unlink(public_path('storage/' . $message->voice));
-                $voice = $request->voice;
-                $filename = "Voice-" . time() . "-" . rand() . "." . $voice->getClientOriginalExtension();
-                $voice->storeAs('message', $filename, "public");
 
-                $messageData['voice'] = "message/" . $filename;
+            if ($type == 'message') {
+                $messageData['message'] = $request->message ?? '';
+                $message->update($messageData);
+                $messages[]['id'] = $message->id;
             }
 
-            $message->update($messageData);
+            if ($type == 'voice') {
+                if (is_array($request->message)) {
+                    foreach ($request->message as $voices) {
+                        $voice = $voices;
+                        $filename = "Voice-" . time() . "-" . rand() . "." . $voice->getClientOriginalExtension();
+                        $voice->storeAs('voice', $filename, "public");
+                        $messageData['message'] = "voice/" . $filename;
+                        $message->update($messageData);
+                        $messages[]['id'] = $message->id;
 
+                    }
+                } else {
+                    $voice = $request->message;
+                    $filename = "Voice-" . time() . "-" . rand() . "." . $voice->getClientOriginalExtension();
+                    $voice->storeAs('voice', $filename, "public");
+                    $messageData['message'] = "voice/" . $filename;
+                    $message->update($messageData);
+                    $messages[]['id'] = $message->id;
+
+                }
+            }
+
+            if ($type == 'image') {
+                if (is_array($request->message)) {
+                    foreach ($request->message as $images) {
+                        $image = $images;
+                        $filename = "Image-" . time() . "-" . rand() . "." . $image->getClientOriginalExtension();
+                        $image->storeAs('image', $filename, "public");
+                        $messageData['message'] = "image/" . $filename;
+                        $message->update($messageData);
+                        $messages[]['id'] = $message->id;
+
+                    }
+                } else {
+                    $image = $request->message;
+                    $filename = "Image-" . time() . "-" . rand() . "." . $image->getClientOriginalExtension();
+                    $image->storeAs('image', $filename, "public");
+                    $messageData['message'] = "image/" . $filename;
+                    $message->update($messageData);
+                    $messages[]['id'] = $message->id;
+
+                }
+            }
+
+            if ($type == 'video') {
+                if (is_array($request->message)) {
+                    foreach ($request->message as $videos) {
+                        $video = $videos;
+                        $filename = "Video-" . time() . "-" . rand() . "." . $video->getClientOriginalExtension();
+                        $video->storeAs('video', $filename, "public");
+                        $messageData['message'] = "video/" . $filename;
+                        $message->update($messageData);
+                        $messages[]['id'] = $message->id;
+
+                    }
+                } else {
+                    $video = $request->message;
+                    $filename = "Video-" . time() . "-" . rand() . "." . $video->getClientOriginalExtension();
+                    $video->storeAs('video', $filename, "public");
+                    $messageData['message'] = "video/" . $filename;
+                    $message->update($messageData);
+                    $messages[]['id'] = $message->id;
+
+                }
+            }
+            dd($messages);
+            $messageIds = array_column($messages, 'id');
+            $mess = Message::whereIn('id', $messageIds)->get();
             DB::commit();
             return response()->json([
                 'status' => true,
                 'message' => "Message Send Successfully",
-                'messages' => new AllMessageResource($message)
+                'messages' => AllMessageResource::collection($mess)
             ]);
         } catch (Throwable $th) {
             DB::rollBack();
